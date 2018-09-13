b2d81c067b60decc8c05580091e8876b4b7971ab
tao.dong <tao.dong@ruisdata.com>
2018-09-13 14:14:35 +0800

    Update : LocalDate and LocalTime and LocalDateTime

--- java-learn/java-eight/src/main/java/com/tao/time/Time.java
+++ java-learn/java-eight/src/main/java/com/tao/time/Time.java
@@ -1,12 +1,13 @@
 package com.tao.time;
 
+import java.time.DayOfWeek;
+import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Period;
 import java.time.ZoneId;
-import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalAdjusters;
@@ -19,69 +20,63 @@ import java.util.Date;
 public class Time {
 
   public static void main(String[] args) {
+
     LocalDate localDate = LocalDate.now();
+    LocalTime localTime = LocalTime.now();
+    LocalDateTime localDateTime = LocalDateTime.now();
 
-    LocalDate ofDate = LocalDate.of(2018, 11, 1);
-    LocalDate firstDay = localDate.with(TemporalAdjusters.firstDayOfMonth());
-    LocalDate lastDay = localDate.withDayOfMonth(1);
-    LocalDate plusDay = localDate.plusDays(-1);
-    boolean leapYear = localDate.isLeapYear();
+    System.out.println("localData is " + localDate);
+    System.out.println("localTime is " + localTime);
+    System.out.println("localDataTime is " + localDateTime);
 
-    System.out.println(localDate);
-    System.out.println(ofDate);
-    System.out.println(firstDay);
-    System.out.println(lastDay);
-    System.out.println(plusDay);
-    System.out.println(leapYear);
-    System.out.println("**************************");
+    System.out.println(localTime.withNano(0));  // 去掉纳秒
+    System.out.println(localDate.withYear(1996).withMonth(5).withDayOfMonth(13)); // 调整时间
+    System.out.println(localDate.with(TemporalAdjusters.firstDayOfNextMonth()));  // 下月第一天
+    System.out.println(localDate.with(TemporalAdjusters.next(DayOfWeek.MONDAY))); // 下个星期一，不包括今天
+    System.out
+        .println(localDate.with(TemporalAdjusters.nextOrSame(DayOfWeek.MONDAY))); // 下个星期一，包括今天
 
-    LocalTime nowTime = LocalTime.now();
-    LocalTime parseTime = LocalTime.parse("12:00:23");
+    LocalDate ofData = LocalDate.of(2018, 8, 15);
+    System.out.println(localDate.isLeapYear());  //闰年
+    System.out.println(localDate.isBefore(ofData));
+    System.out.println(localDate.isAfter(ofData));
+    System.out.println(localDate.plusDays(1)); // +1 天
+    System.out.println(localDate.plusDays(-1)); //  -1 天
+    System.out.println(localDate.minusDays(1)); //  -1 天
+    System.out.println(localDate.plus(2, ChronoUnit.DAYS)); // // +2 天
+    System.out.println(localTime.withNano(0));
 
-    System.out.println(nowTime);
-    System.out.println(nowTime.withNano(0));
-    System.out.println(parseTime);
-    System.out.println(parseTime.plus(-2, ChronoUnit.HOURS));
-
-    System.out.println("**********************");
-
-    ZoneId defaultZone = ZoneId.systemDefault();
-    LocalDate today = LocalDate.now();
-    LocalDate specifyDate = LocalDate.of(1996, 5, 13);
-    Period between = Period.between(specifyDate, today);
-
-    System.out.println(defaultZone);
-    System.out.println();
-    System.out.println(between.getDays());
-    System.out.println(between.getMonths());
-    System.out.println(specifyDate.until(today, ChronoUnit.DAYS));
-
-    System.out.println("********************");
-
-    String date = "20180710";
-    DateTimeFormatter formatter = DateTimeFormatter.BASIC_ISO_DATE;
-    DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("YYYY MM dd");
-    LocalDate formatDate = LocalDate.parse(date, formatter);
-    System.out.println(formatDate);
-    System.out.println(formatter2.format(LocalDate.now()));
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy MM dd");
+    System.out.println(LocalDate.parse("2018 03 12", formatter));
     System.out.println(formatter.format(LocalDate.now()));
 
-    System.out.println("*******************************");
+    //获得所有可用的时区  size=595
+    ZoneId.getAvailableZoneIds();
+    //获取默认ZoneId对象
+    ZoneId.systemDefault();
+    //获取指定时区的ZoneId对象
+    ZoneId.of("Asia/Shanghai");
+    //ZoneId.SHORT_IDS返回一个Map<String, String> 是时区的简称与全称的映射。下面可以得到字符串 Asia/Shanghai
+    ZoneId.SHORT_IDS.get("CTT");
 
-    Instant instant = localDate.atStartOfDay(ZoneId.systemDefault()).toInstant();
+    Instant instant = localDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant();
+    Instant instant1 = localDateTime.atZone(ZoneId.systemDefault()).toInstant();
+    System.out.println(instant.toEpochMilli());
+    System.out.println(instant1.toEpochMilli());
+
+    LocalDate specifyDate = LocalDate.of(2018, 9, 13);
+    LocalTime specifyTime = LocalTime.of(14, 2, 58);
+    Period period = Period.between(specifyDate, localDate); // 天
+    Duration duration = Duration.between(specifyTime,localTime);  // 时分秒
+    System.out.println(period.getDays());
+    System.out.println(period.getMonths());
+    System.out.println(duration.getSeconds());
+    System.out.println(specifyDate.until(localDate, ChronoUnit.DAYS)); // 获取相距天数，前 - 后
 
     Date date1 = Date.from(instant);
-    System.out.println(instant);
+    Instant instant2 = new Date().toInstant();
     System.out.println(date1);
-    Date date2 = new Date();
-    System.out.println(date2.toInstant().atZone(ZoneId.systemDefault()));
-
-    System.out.println("*******************************");
-
-    DateTimeFormatter formatter3 = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-    LocalDateTime localDateTime = LocalDateTime.parse("2018-09-06 13:12:03",formatter3);
-    System.out.println(localDateTime);
-    System.out.println(localDateTime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli());
+    System.out.println(instant2);
 
   }
 
