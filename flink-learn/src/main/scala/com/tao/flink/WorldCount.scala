efa7c302b3341d94eb6f6f40395c15269446f42a
tao.dong <tao.dong@ruisdata.com>
2021-09-28 15:07:30 +0800

    Update : 更新flink worldcount

--- flink-learn/src/main/scala/com/tao/flink/WorldCount.scala
+++ flink-learn/src/main/scala/com/tao/flink/WorldCount.scala
@@ -1,7 +1,9 @@
 package com.tao.flink
 
+import org.apache.flink.api.java.tuple.Tuple
 import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow
 
 
 /**
@@ -10,22 +12,23 @@ import org.apache.flink.streaming.api.windowing.time.Time
  */
 object WorldCount {
 
-    case class WordWithCount(word: String, count: Long)
+  case class WordWithCount(word: String, count: Long)
 
-    def main(args: Array[String]): Unit = {
-        // 获取执行器的环境
-        val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+  def main(args: Array[String]): Unit = {
 
-        //获取数据: 从socket中获取
-        val textDataStream = env.socketTextStream("127.0.0.1", 8888, '\n')
-        val tupDataStream = textDataStream.flatMap(_.split(" ")).map(WordWithCount(_, 1))
+    // 获取执行器的环境
+    val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
 
-        //groupby: 按照指定的字段聚合
-        val windowDstram = tupDataStream.keyBy("word").timeWindow(Time.seconds(5), Time.seconds(1)) //窗口bsize=5秒, slid=1s
-        windowDstram.sum("count").print()
+    //获取数据: 从socket中获取 nc -lk 8888
+    val textDataStream = env.socketTextStream("127.0.0.1", 8888, '\n')
+    val tupDataStream = textDataStream.flatMap(_.split(" ")).map(WordWithCount(_, 1))
+
+    //group by: 按照指定的字段聚合
+    val windowedStream: WindowedStream[WordWithCount, Tuple, TimeWindow] = tupDataStream.keyBy("word").timeWindow(Time.seconds(5), Time.seconds(1)) //窗口bsize=5秒, slid=1s
+    windowedStream.sum("count").print()
 
 
-        env.execute("Socket Window WordCount")
-    }
+    env.execute("Socket Window WordCount")
+  }
 
 }
